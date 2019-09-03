362434b8dfb90b8a9d7327c0a21cff985a63ff47
Sage_of_Mirrors <empressglatia@gmail.com>
2019-09-03 15:38:39 -0500

    Removed duplicate and outdated GBA files; all GBA functionality is contained within libgbacom.

--- PC-TCG/include/Constants.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-constexpr auto BITS_PER_SECOND = 115200;
-constexpr auto BYTES_PER_SECOND = BITS_PER_SECOND / 8;
-constexpr auto SEND_MAX_SIZE = 5, RECV_MAX_SIZE = 5;
-constexpr auto TICKS_PER_SECOND = 486000000u;
